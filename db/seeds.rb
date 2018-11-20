9ec7dd08ff6ef0286e60add600118b233113ac8d
Alix Taffle <ataffle@gmail.com>
2018-11-20 17:08:19 +0100

    completed rail-task-manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+task_purchase = Task.create({ title: "Faire les courses", details: "lait, PQ, PQ, PQ"})
+task_clean = Task.create({ title: "Faire le ménage", details: "vitres, aspirateur,..."})
+task_enjoy = Task.create({ title: "Boire des bières", details: "Villa Gaudelet"})
+
